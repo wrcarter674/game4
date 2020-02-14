51cb47a27382584641806f3dce65671207d6e6c7
amandajoelleyoung <nhbean@ksu.edu>
2020-02-14 11:22:05 -0600

    Added collision library

--- PlatformerExample/Player.cs
+++ PlatformerExample/Player.cs
@@ -9,6 +9,9 @@ using Microsoft.Xna.Framework.Input;
 
 namespace PlatformerExample
 {
+    /// <summary>
+    /// An enumeration of possible player animation states
+    /// </summary>
     enum PlayerAnimState
     {
         Idle,
@@ -19,54 +22,113 @@ namespace PlatformerExample
         FallingLeft,
         FallingRight
     }
+
+    /// <summary>
+    /// A class representing the player
+    /// </summary>
     public class Player
     {
+        // The speed of the walking animation
         const int FRAME_RATE = 300;
 
-        SpriteEffects spriteEffects = SpriteEffects.None;
+        // The duration of a player's jump, in milliseconds
+        const int JUMP_TIME = 500;
 
+        // The player sprite frames
         Sprite[] frames;
 
+        // The currently rendered frame
         int currentFrame = 0;
 
+        // The player's animation state
         PlayerAnimState animationState = PlayerAnimState.Idle;
 
+        // The player's speed
         int speed = 3;
 
+        // If the player is jumping
         bool jumping = false;
 
+        // If the player is falling 
+        bool falling = false;
+
+        // A timer for jumping
+        TimeSpan jumpTimer;
+
+        // A timer for animations
         TimeSpan animationTimer;
 
+        // The currently applied SpriteEffects
+        SpriteEffects spriteEffects = SpriteEffects.None;
+
+        // The color of the sprite
         Color color = Color.White;
 
+        // The origin of the sprite (centered on its feet)
         Vector2 origin = new Vector2(10, 21);
 
+        /// <summary>
+        /// Gets and sets the position of the player on-screen
+        /// </summary>
         public Vector2 Position = new Vector2(200, 200);
 
-
+        /// <summary>
+        /// Constructs a new player
+        /// </summary>
+        /// <param name="frames">The sprite frames associated with the player</param>
         public Player(IEnumerable<Sprite> frames)
         {
             this.frames = frames.ToArray();
             animationState = PlayerAnimState.WalkingLeft;
         }
 
+        /// <summary>
+        /// Updates the player, applying movement and physics
+        /// </summary>
+        /// <param name="gameTime">The GameTime object</param>
         public void Update(GameTime gameTime)
         {
             var keyboard = Keyboard.GetState();
 
-            if (!jumping && keyboard.IsKeyDown(Keys.Space)) 
+            // Vertical movement
+            if(jumping)
+            {
+                jumpTimer += gameTime.ElapsedGameTime;
+                // TODO: Replace jumping with platformer physics
+                //Position.Y -= speed; // Naive jumping 
+                Position.Y -= (250 / (float)jumpTimer.TotalMilliseconds);
+                if(jumpTimer.TotalMilliseconds >= JUMP_TIME)
+                {
+                    jumping = false;
+                    falling = true;
+                }
+            }
+            if(falling)
+            {
+                Position.Y += speed;
+                // TODO: This needs to be replaced with collision logic
+                if (Position.Y > 400)
+                {
+                    Position.Y = 400;
+                    falling = false;
+                }
+            }
+            if (!jumping && !falling && keyboard.IsKeyDown(Keys.Space)) 
             { 
                 jumping = true;
+                jumpTimer = new TimeSpan(0);
             }
+
+            // Horizontal movement
             if (keyboard.IsKeyDown(Keys.Left))
             {
-                if (jumping) animationState = PlayerAnimState.JumpingLeft;
+                if (jumping || falling) animationState = PlayerAnimState.JumpingLeft;
                 else animationState = PlayerAnimState.WalkingLeft;
                 Position.X -= speed;
             }
             else if(keyboard.IsKeyDown(Keys.Right))
             {
-                if (jumping) animationState = PlayerAnimState.JumpingRight;
+                if (jumping || falling) animationState = PlayerAnimState.JumpingRight;
                 else animationState = PlayerAnimState.WalkingRight;
                 Position.X += speed;
             }
@@ -75,6 +137,7 @@ namespace PlatformerExample
                 animationState = PlayerAnimState.Idle;
             }
 
+            // Apply animations
             switch(animationState)
             {
                 case PlayerAnimState.Idle:
@@ -113,6 +176,11 @@ namespace PlatformerExample
             }
         }
 
+        /// <summary>
+        /// Render the player sprite.  Should be invoked between 
+        /// SpriteBatch.Begin() and SpriteBatch.End()
+        /// </summary>
+        /// <param name="spriteBatch">The SpriteBatch to use</param>
         public void Draw(SpriteBatch spriteBatch)
         {
             frames[currentFrame].Draw(spriteBatch, Position, color, 0, origin, 2, spriteEffects, 1);
